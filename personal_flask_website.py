aa0279448d9ba16de3da32dbe8d7eb16cd6addbb
v-alan-m <muhunthanv9@gmail.com>
2019-12-16 17:03:11 +0000

    comments added

--- personal_flask_website.py
+++ personal_flask_website.py
@@ -1,10 +1,14 @@
 from flask import Flask
 
+#Create an variable to store the Flask app object 
 app = Flask(__name__)
 
+#Create a decorator o view the webiste, # means homepage
 @app.route('/')
+#Create a function to define what they webpage does
 def home():
     return "Website content goes here"
 
+#Conditional statement to run the code 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
